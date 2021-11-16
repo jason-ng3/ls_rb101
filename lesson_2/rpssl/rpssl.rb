301e2f39b59fc6bc616a30a930d5bb3a25445f31
Jason Ng <jasonscoat@yahoo.com>
2021-11-16 16:39:32 -0500

    Update rpssl files

--- lesson_2/rpssl/rpssl.rb
+++ lesson_2/rpssl/rpssl.rb
@@ -1,7 +1,12 @@
 require "yaml"
 
-VALID_INITIALS = ["r", "p", "sc", "sp", "l"]
-VALID_CHOICES = ["Rock", "Paper", "Scissors", "Spock", "Lizard"]
+VALID_CHOICES = { 
+  "r" => "Rock",
+  "p" => "Paper",
+  "sc" => "Scissors",
+  "sp" => "Spock",
+  "l" => "Lizard"
+}
 WINNING_COMBINATIONS = {
   Rock: [:Scissors, :Lizard],
   Paper: [:Rock, :Spock],
@@ -15,12 +20,51 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_initials?(choice)
-  VALID_INITIALS.include?(choice)
+def get_user_name
+  user_name = ""
+  prompt "Enter your name: "
+
+  loop do
+    user_name = gets.chomp
+    break unless user_name.empty?
+    prompt "Please enter a valid name: "
+  end
+
+  user_name
+end
+
+def get_user_choice(user_name)
+  user_choice = ""
+  system "clear"
+
+  loop do
+    prompt "#{user_name}, choose one letter:"
+    VALID_CHOICES.each do |letter, choice|
+      puts "#{letter} for #{choice}"
+    end
+
+    user_choice = gets.chomp.downcase
+    break if valid_choice?(user_choice)
+    prompt "Invalid choice."
+  end
+
+  user_choice
+end
+
+def valid_choice?(choice)
+  VALID_CHOICES.keys.include?(choice)
 end
 
 def win?(player1, player2)
   WINNING_COMBINATIONS[player1].include?(player2)
+end
+
+def update_score(user_choice, computer_choice, score)
+  if win?(user_choice, computer_choice)
+    score[:user_wins] += 1
+  elsif win?(computer_choice, user_choice)
+    score[:computer_wins] += 1
+  end
 end
 
 def display_result(user_choice, computer_choice, user_name)
@@ -31,95 +75,59 @@ def display_result(user_choice, computer_choice, user_name)
   else
     prompt "#{user_choice} ties #{computer_choice}, it's a draw!."
   end
+  sleep 3
 end
 
-def display_score(user_wins, computer_wins, user_name)
-  if user_wins >= computer_wins
-    prompt "#{user_name} #{user_wins}, Computer #{computer_wins}"
+def display_score(score, user_name)
+  if score[:user_wins] >= score[:computer_wins]
+    prompt "#{user_name} #{score[:user_wins]}, Computer #{score[:computer_wins]}"
   else
-    prompt "Computer #{computer_wins}, #{user_name} #{user_wins}"
+    prompt "Computer #{score[:computer_wins]}, #{user_name} #{score[:user_wins]}"
   end
+  sleep 3
 end
 
-def display_champion(user_wins, computer_wins, user_name)
-  if user_wins == 3
+def display_champion(score, user_name)
+  if score[:user_wins] == 3
     prompt "#{user_name} is the champion of Rock Paper Scissors Spock Lizard!"
-  elsif computer_wins == 3
+  elsif score[:computer_wins] == 3
     prompt "Computer is the champion of Rock Paper Scissors Spock Lizard!"
   end
+  sleep 1
 end
+
+def play_again?
+  prompt "Play another match? (y)"
+  answer = gets.chomp.downcase
+  answer.start_with?("y")
+end
+
+# Program starts here
 
 prompt "Welcome to Rock Paper Scissors Spock Lizard!"
 sleep 3
-prompt "Enter your name: "
-
-user_name = ""
-loop do
-  user_name = gets.chomp
-  break unless user_name.empty?
-  prompt "Please enter a valid name: "
-end
-
+user_name = get_user_name
 prompt "Hi #{user_name}! First to 3 wins is the champion. Match begins now!"
 sleep 4
 
 loop do
-  user_wins = 0
-  computer_wins = 0
+  score = { user_wins: 0, computer_wins: 0 }
 
   loop do
-    user_choice = ""
-    loop do
-      system "clear"
-      choices = <<~MSG
-      #{user_name}, choose one letter:
-      r (Rock)
-      p (Paper)
-      sc (Scissors)
-      sp (Spock)
-      l (Lizard)
-      MSG
+    user_choice = VALID_CHOICES[get_user_choice(user_name)].to_sym
+    computer_choice = VALID_CHOICES.values.sample.to_sym
 
-      prompt choices
-      user_choice = gets.chomp.downcase
-      break if valid_initials?(user_choice)
-      prompt "Invalid choice."
-    end
-
-    case user_choice
-    when "r" then user_choice = "Rock"
-    when "p" then user_choice = "Paper"
-    when "sc" then user_choice = "Scissors"
-    when "sp" then user_choice = "Spock"
-    when "l" then user_choice = "Lizard"
-    end
-
-    computer_choice = VALID_CHOICES.sample
-    sleep 3
     prompt "#{user_name} chose #{user_choice}; Computer chose #{computer_choice}."
     sleep 3
 
-    user_choice = user_choice.to_sym
-    computer_choice = computer_choice.to_sym
-
-    if win?(user_choice, computer_choice)
-      user_wins += 1
-    elsif win?(computer_choice, user_choice)
-      computer_wins += 1
-    end
-
     display_result(user_choice, computer_choice, user_name)
-    sleep 3
-    display_score(user_wins, computer_wins, user_name)
-    sleep 3
-    display_champion(user_wins, computer_wins, user_name)
-    sleep 1
-    break if user_wins == 3 || computer_wins == 3
+    update_score(user_choice, computer_choice, score)
+    display_score(score, user_name)
+    display_champion(score, user_name)
+    break if score[:user_wins] == 3 || score[:computer_wins] == 3
   end
 
-  prompt "Play another match? (y)"
-  answer = gets.chomp.downcase
-  break unless answer.start_with?("y")
+  break unless play_again?
 end
 
-prompt "Thank you #{user_name} for playing Rock Paper Scissors Spock Lizard!"
+prompt "Thank you #{user_name} for playing!"
